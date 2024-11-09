872c4d9561ab7e03e55518273d14eccc6ace7478
felipeanselmonascimento <felipeanselmodonascimento@gmail.com>
2024-11-09 15:50:18 +0000

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold }) //passando as fonts q quero carregar // primeiro valor booleano eu sei se font foi carregada ou n
+  //sempre checar se a font foi carregada ou n
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#202024" }}>
+      {fontsLoaded ? <Text>Home</Text> : <View/>}
+      {/* Esse Statusbar e o q fica em cima mostrndo a bateria e etc */}
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/> 
     </View>
+
+   
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
